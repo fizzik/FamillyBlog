3d8530cf2c15a8f441dd009edbf807f5fae38d70
Vitaleks <Vitaleks2010@i.ua>
2012-12-08 13:55:12 +0400

    scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FamillyBlog::Application.config.secret_token = '705ef35cd955733c6762cf7491e509124c3e01e7342fe9ab9c4890ae2119cc4c99fa396d82c6d31ec2edfbc7b6a3b4ee476c0a68f3609b257e120ad0c649bd03'
