0a615e715e1221ec5d351af6ea314d12e6461d2c
Vitaleks <Vitaleks2010@i.ua>
2012-12-17 21:37:31 +0400

    info_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessor :password
-  attr_accessible :name, :email, :password, :password_confirmation, :image, :remove_image
+  attr_accessible :name, :email, :password, :password_confirmation, :image, :remove_image, :skype, :phone, :date_of_birth, :town, :about
   mount_uploader :image,ImageUploader
 
   has_many :comments
--- app/models/user_info.rb
+++
@@ -1,4 +0,0 @@
-class UserInfo < ActiveRecord::Base
-  attr_accessible :about, :date_of_birth, :phone, :skype, :town
-  has_many :users
-end
