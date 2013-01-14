8d4f24b6ecf15c35f0408233732caee92f9382b7
VitaleksPC <vitalekspc@ubuntu.(none)>
2013-01-14 19:30:20 +0400

    heroku

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # Load the rails application
 Arugil1 git:(master) ✗ more config/environment.rb 
 require File.expand_path('../application', __FILE__)
+ActiveSupport::Deprecation.silenced = true 
 
 
 # Initialize the rails application
