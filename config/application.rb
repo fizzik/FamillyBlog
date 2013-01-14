7e37ccc5393419bed4bd56eb9da21931d9cc45d1
VitaleksPC <vitalekspc@ubuntu.(none)>
2013-01-14 19:57:54 +0400

    heriku

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ end
 module FamillyBlog
   class Application < Rails::Application
  config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
